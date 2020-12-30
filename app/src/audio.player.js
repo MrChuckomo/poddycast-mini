f47b705e19c2afb71ac211ba3078695ddae72980
MrChuckomo <alexander.kettler91@gmail.com>
2020-12-30 20:46:40 +0100

    Progress
    # make the progress work
    # update progress while episode is playing

--- app/src/audio.player.js
+++ app/src/audio.player.js
@@ -10,12 +10,28 @@ function startPlayback () {
     const player = document.getElementById('player')
 
     player.play()
+    player.addEventListener('timeupdate', updateProgress, false)
 }
 
 function pausePlayback () {
     const player = document.getElementById('player')
 
     player.pause()
+}
+
+function updateProgress () {
+    const player = document.getElementById('player')
+    const playerSource = player.getElementsByTagName('source')[0]
+    let value = 0
+
+    if (player.currentTime > 0) {
+        value = Math.floor((100 / player.duration) * player.currentTime)
+    }
+
+    const progress = document.getElementById('playerProgress')
+
+    progress.style.width = value + '%'
+    console.log(value)
 }
 
 function setupArtwork (artwork) {
