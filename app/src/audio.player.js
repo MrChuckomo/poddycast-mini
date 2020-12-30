76bdc0f2effdf2cbd0851461e7da1d95b3c05e1b
MrChuckomo <alexander.kettler91@gmail.com>
2020-12-30 21:15:59 +0100

    Playback time
    # format time
    # show playback time (current position)

--- app/src/audio.player.js
+++ app/src/audio.player.js
@@ -19,9 +19,15 @@ function pausePlayback () {
     player.pause()
 }
 
+/**
+ * Creates a new Circle from a diameter.
+ *
+ * @param {number} d The desired diameter of the circle.
+ * @return {Circle} The new Circle object.
+ */
 function updateProgress () {
     const player = document.getElementById('player')
-    const playerSource = player.getElementsByTagName('source')[0]
+    const playerTime = document.getElementById('playerPlaybackTime')
     let value = 0
 
     if (player.currentTime > 0) {
@@ -31,9 +37,17 @@ function updateProgress () {
     const progress = document.getElementById('playerProgress')
 
     progress.style.width = value + '%'
-    console.log(value)
+
+    const formatTime = getFromatTime(player.currentTime)
+
+    playerTime.innerText = formatTime.hours + ':' + formatTime.minutes + '.' + formatTime.seconds
 }
 
+/**
+ * Creates a new Circle from a diameter.
+ *
+ * @param {string} artwork The desired diameter of the circle.
+ */
 function setupArtwork (artwork) {
     document.getElementById('playerArtwork').setAttribute('src', artwork)
 }
@@ -44,4 +58,33 @@ function setupCollectionName (collectionName) {
 
 function setupTitle (title) {
     document.getElementById('playerTitle').innerText = title
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Creates a new Circle from a diameter.
+ *
+ * @param {number} timeInSeconds The desired diameter of the circle.
+ * @return {dict} The new Circle object.
+ */
+function getFromatTime (timeInSeconds) {
+    const fullTime = {}
+
+    const hours = Math.floor(timeInSeconds / 3600)
+
+    timeInSeconds = timeInSeconds - (hours * 3600)
+
+    const minutes = Math.floor(timeInSeconds / 60)
+    const seconds = Math.floor(timeInSeconds - (minutes * 60))
+
+    fullTime.hours = getPrettyTime(hours)
+    fullTime.minutes = getPrettyTime(minutes)
+    fullTime.seconds = getPrettyTime(seconds)
+
+    return fullTime
+}
+
+function getPrettyTime (number) {
+    return ((number < 10) ? '0' + number : number)
 }
